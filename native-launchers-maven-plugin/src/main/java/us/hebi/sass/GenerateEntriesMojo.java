2c419b0855410d098591d69f7ddde9834dc051ba
Florian Enner <florian@hebirobotics.com>
2023-06-09 09:59:20 +0200

    copied basic maven plugin setup

---
+++ native-launchers-maven-plugin/src/main/java/us/hebi/sass/GenerateEntriesMojo.java
@@ -0,0 +1,41 @@
+/*-
+ * #%L
+ * sass-cli-maven-plugin Maven Mojo
+ * %%
+ * Copyright (C) 2022 HEBI Robotics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package us.hebi.sass;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @author Florian Enner
+ * @since 30 Jul 2022
+ */
+@Mojo(name = "gen-entries")
+@Execute(phase = LifecyclePhase.COMPILE)
+public class GenerateEntriesMojo extends BaseMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        getLog().info("Generating entries");
+    }
+
+}
