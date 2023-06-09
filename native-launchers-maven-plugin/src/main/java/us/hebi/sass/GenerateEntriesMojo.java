4ffac2e724bfedb15c37b28f0657613e42961de4
Florian Enner <florian@hebirobotics.com>
2023-06-09 12:52:09 +0200

    added proof of concept using ByteBuddy

--- native-launchers-maven-plugin/src/main/java/us/hebi/sass/GenerateEntriesMojo.java
+++ native-launchers-maven-plugin/src/main/java/us/hebi/sass/GenerateEntriesMojo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,23 @@
 
 package us.hebi.sass;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.ClassFileVersion;
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.Implementation;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointerPointer;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
 
 /**
  * @author Florian Enner
@@ -36,6 +49,45 @@ public class GenerateEntriesMojo extends BaseMojo {
     @Override
     public void execute() throws MojoExecutionException {
         getLog().info("Generating entries");
+
+        // Generate compiled class contents so that users don't need to compile against graal libs.
+        // Unfortunately, it looks like GraalVM requires a source file, so the pre-compiled approach
+        // may not work. This attempt fails with:
+        //
+        //      Fatal error: java.lang.NullPointerException: Cannot invoke "String.compareTo(String)"
+        //      because the return value of "jdk.vm.ci.meta.ResolvedJavaType.getSourceFileName()" is null
+
+        DynamicType.Builder<?> clazz = new ByteBuddy(ClassFileVersion.JAVA_V8)
+                .subclass(Object.class)
+                .name("graal.Entries");
+
+        for (Launcher launcher : launchers) {
+
+            Implementation impl = FixedValue.value(0);
+
+            AnnotationDescription cEntryAnnotation = AnnotationDescription.Builder.ofType(CEntryPoint.class)
+                    .define("name", launcher.getConventionalName())
+                    .build();
+
+            clazz = clazz.defineMethod(launcher.getConventionalName(), int.class, Modifier.PUBLIC | Modifier.STATIC)
+                    .withParameters(IsolateThread.class, int.class, CCharPointerPointer.class)
+                    .intercept(impl)
+                    .annotateMethod(cEntryAnnotation);
+
+        }
+
+        // write to a .class file
+        try {
+            clazz.make().saveIn(new File(classOutputDirectory));
+        } catch (IOException e) {
+            throw new MojoExecutionException(e);
+        }
+
+
+        System.out.println(classOutputDirectory);
+
+        getLog().error(String.valueOf(launchers));
+
     }
 
 }
