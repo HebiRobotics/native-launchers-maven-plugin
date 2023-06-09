3c4dbc13c532519adf6763b4c4695b4beaea67c7
Florian Enner <florian@hebirobotics.com>
2023-06-09 12:49:37 +0200

    added initial maven parameters

--- native-launchers-maven-plugin/src/main/java/us/hebi/sass/BaseMojo.java
+++ native-launchers-maven-plugin/src/main/java/us/hebi/sass/BaseMojo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,14 +20,58 @@
 
 package us.hebi.sass;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
 
 /**
  * @author Florian Enner
  * @since 09 Jun 2023
  */
 abstract class BaseMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${plugin}", readonly = true) // Maven 3 only
+    protected PluginDescriptor plugin;
+
+    @Parameter(defaultValue = "${session}", readonly = true)
+    protected MavenSession session;
+
+    // https://github.com/graalvm/native-build-tools/blob/master/native-maven-plugin/src/main/java/org/graalvm/buildtools/maven/AbstractNativeImageMojo.java#LL106C28-L106C37
+    @Parameter(property = "imageName", defaultValue = "${project.artifactId}", required = true)
+    protected String imageName;
+
+    @Parameter(property = "imageDirectory", defaultValue = "${project.build.directory}", required = true)
+    protected String imageDirectory;
+
+    @Parameter(property = "classOutputDirectory", defaultValue = "${project.build.outputDirectory}", required = true)
+    protected String classOutputDirectory;
+
+    @Parameter(property = "launchers", required = true)
+    protected List<Launcher> launchers;
+
+    public static class Launcher {
+
+        @Parameter(property = "name", required = true)
+        protected String name;
+
+        @Parameter(property = "mainClass", required = true)
+        protected String mainClass;
+
+        @Parameter(property = "imageName", required = false)
+        protected String imageName;
+
+        @Parameter(defaultValue = "${project.build.directory}", required = false)
+        protected String imageDirectory;
+
+        public String getConventionalName() {
+            return "run_" + mainClass.replaceAll("\\.", "_");
+        }
+
+    }
 
 }
