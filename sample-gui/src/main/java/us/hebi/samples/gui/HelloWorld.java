1d2a5a6528ffc0e47c078ec155223fd214603fc1
Florian Enner <florian@hebirobotics.com>
2023-06-10 13:25:12 +0200

    attempt at creating a shared library w/ JavaFX

--- sample-gui/src/main/java/us/hebi/samples/gui/HelloWorld.java
+++ sample-gui/src/main/java/us/hebi/samples/gui/HelloWorld.java
@@ -1,11 +1,20 @@
 package us.hebi.samples.gui;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * @author Florian Enner
  * @since 08 Jun 2023
  */
-public class HelloWorld {
+public class HelloWorld extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
 }
